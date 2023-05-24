6fe4b67f211cbfb2b8a711e4ea399727ba106ca2
neverGiveUppppp <csy6219@naver.com>
2023-05-24 18:21:01 +0900

    Solve : baekjoon

--- Baekjoon/src/com/baekjoon/step3/Step3_2023.java
+++ Baekjoon/src/com/baekjoon/step3/Step3_2023.java
@@ -1271,18 +1271,34 @@ brainstorming
     }
 /*
     정답
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 import java.io.*;
+import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) {
+          try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            StringTokenizer st = null;
+            String str;
+            while((str = br.readLine()) != null) {
+                st = new StringTokenizer(str," ");
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
 
+                if( (0 < a && a < 10) && (0 < b && b < 10) ) {
+//                bw.write(String.valueOf(a + b + "\n"));
+                    bw.write(a + b + "\n");
+                }
+            }
+            br.close();
+            bw.flush();
+            bw.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
+
 
  */
 
