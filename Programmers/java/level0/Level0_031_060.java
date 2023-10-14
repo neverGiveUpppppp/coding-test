9c0f89eceef0cf003d8648ddca35b1f5e6011765
neverGiveUpppppp <csy6219@naver.com>
2023-10-14 19:16:38 +0900

    Solve : java level0 짝수는 싫어요 주어진 수의 모든 홀수를 구하는 문제 1.주어진 수 n의 홀수의 개수를 계산 2.n도 홀수면 개수에 포함하기 3.홀수 개수만큼 배열을 생성하고 만들어 넣어주기

--- Programmers/java/level0/Level0_031_060.java
+++ Programmers/java/level0/Level0_031_060.java
@@ -436,7 +436,7 @@ class Solution037 {
 //        }
 
         // 방법2. String.indexOf() : 찾는 문자열이 존재하면 해당 문자열의 인덱스를 반환하고, 없으면 -1을 반환
-        if (str1.indexOf(str2) != -1) {
+        if (str1.indexOf(str2) != -1) { // 응용 : char boolean처럼 사용 할 수 있을 듯함
             answer = 1;
         }else{
             answer = 2;
@@ -470,9 +470,10 @@ class Solution {
  */
 
 
-// level0 :
+// level0 : 짝수는 싫어요
 /*
 조건
+    정수 n이 매개변수로 주어질 때, n 이하의 홀수가 오름차순으로 담긴 배열을 return
     1.
     2.
 
@@ -484,12 +485,28 @@ brainstorming
 class Solution038 {
     public static void main(String[] args) {
         Solution038 prbs = new Solution038();
-        int answer = prbs.solution(0, 1);
+        int[] answer = prbs.solution(15);
         System.out.println(answer);
     }
 
-    public int solution(int slice, int n) {
-        int answer = 0;
+    public int[] solution(int n) {
+        int odd = 0;
+        // 1)홀수 개수 구하기
+        for(int i = 0; i < n; i++){
+            if(i % 2 != 0){
+                odd++;
+            }
+        }
+        // 2)n이 홀수면 n까지 포함하기
+        if(n % 2 != 0){
+            odd++;
+        }
+        // 3)홀수 만들어 넣기
+        int[] answer = new int[odd];
+        answer[0] = 1;
+        for(int i = 1; i <= odd-1; i++){
+            answer[i] = (2 * i) + 1;
+        }
         return answer;
     }
 }
